a202481638704dbe9a963f0ec3e9b8942f68f96c
Vladislav Shamrin <shamrin2007@mail.ru>
2022-10-18 17:55:22 +0300

    Created 'app_constants' include 'initialColorPalette'

--- lib/bloc/color_palette_bloc/color_palette_state.dart
+++ lib/bloc/color_palette_bloc/color_palette_state.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rgb_control/utils/app_constants.dart';
 
 abstract class ColorPaletteState {
   final Color pickerColor;
@@ -7,7 +8,7 @@ abstract class ColorPaletteState {
 }
 
 class ColorPaletteInitialState extends ColorPaletteState {
-  ColorPaletteInitialState() : super(pickerColor: const Color(0xffffffff));
+  ColorPaletteInitialState() : super(pickerColor: Styles.initialColorPalette);
 }
 
 class ColorPaletteSetState extends ColorPaletteState {
---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Texts
+class Texts {
+
+}
+
+// Styles
+class Styles {
+  static const Color initialColorPalette = Color(0xffffffff);
+}
