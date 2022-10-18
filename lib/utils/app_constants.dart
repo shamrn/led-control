f4f2f4f35533a78e66ea8f53a3c2746016858e06
Vladislav Shamrin <shamrin2007@mail.ru>
2022-10-18 18:50:46 +0300

    Updated app constants

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 
 // Texts
 class Texts {
-
+  static const String brightnessLevelTitle = 'Яркость';
 }
 
 // Styles
 class Styles {
-  static const Color initialColorPalette = Color(0xffffffff);
+  static const Color primaryColor = Color(0xfff4fbff);
+  static const Color secondColor = Color(0xff2185ca);
+
+  static const Color initialColorPalette = Color(0xff416981);
+
+  static const TextStyle primaryTextStyle =
+      TextStyle(color: primaryColor, fontSize: 22);
 }
