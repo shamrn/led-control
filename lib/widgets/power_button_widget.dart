81bcb6dfca397fd5f48cf4f3a78b96ee9d883670
Vladislav Shamrin <shamrin2007@mail.ru>
2022-10-23 13:12:30 +0300

    For power button added vibration

--- lib/widgets/power_button_widget.dart
+++ lib/widgets/power_button_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rgb_control/bloc/color_bloc/color_bloc.dart';
 import 'package:rgb_control/bloc/color_bloc/color_state.dart';
@@ -12,7 +13,6 @@ class PowerButtonWidget extends StatefulWidget {
 }
 
 class _PowerButtonWidgetState extends State<PowerButtonWidget> {
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -20,19 +20,21 @@ class _PowerButtonWidgetState extends State<PowerButtonWidget> {
       child: BlocBuilder<ColorPowerBloc, ColorState>(
         builder: (context, state) => ClipOval(
             child: Material(
-          color:
-              state is ColorPowerOnState ? Styles.secondColor : Styles.primaryColor,
+          color: state is ColorPowerOnState
+              ? Styles.secondColor
+              : Styles.primaryColor,
           child: InkWell(
             splashColor: state is ColorPowerOnState
                 ? Styles.primaryColor
                 : Styles.secondColor,
             splashFactory: InkRipple.splashFactory,
             onTap: () {
+              HapticFeedback.vibrate();
               context.read<ColorPowerBloc>().switching(state);
             },
             child: const Icon(
               Icons.offline_bolt_rounded,
-              size: 48,
+              size: 64,
             ),
           ),
         )),
