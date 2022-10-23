e9543f1c6591f0cac60b7d1ceb0ffe4f56248fe2
Vladislav Shamrin <shamrin2007@mail.ru>
2022-10-23 14:40:02 +0300

    'BrightnessLevelWidget' changed on statless widget. Maked 'SliderBrightnessLevel' stateful widget, added in 'BrightnessLevelWidget'

--- lib/widgets/brightness_level_widget.dart
+++ lib/widgets/brightness_level_widget.dart
@@ -4,14 +4,9 @@ import 'package:rgb_control/bloc/color_bloc/color_bloc.dart';
 import 'package:rgb_control/bloc/color_bloc/color_event.dart';
 import 'package:rgb_control/utils/app_constants.dart';
 
-class BrightnessLevelWidget extends StatefulWidget {
+class BrightnessLevelWidget extends StatelessWidget {
   const BrightnessLevelWidget({Key? key}) : super(key: key);
 
-  @override
-  State<BrightnessLevelWidget> createState() => _BrightnessLevelWidgetState();
-}
-
-class _BrightnessLevelWidgetState extends State<BrightnessLevelWidget> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -33,27 +28,41 @@ class _BrightnessLevelWidgetState extends State<BrightnessLevelWidget> {
         const SizedBox(
           height: 20,
         ),
-        SliderTheme(
-          data: SliderThemeData(overlayShape: SliderComponentShape.noOverlay),
-          child: BlocProvider(
-            create: (context) => ColorBrightnessLevelBloc(),
-            child: BlocBuilder<ColorBrightnessLevelBloc, double>(
-              builder: (context, currentLevel) => Slider.adaptive(
-                  value: currentLevel,
-                  min: BrightnessLevel.minLevel,
-                  max: BrightnessLevel.maxLevel,
-                  thumbColor: Styles.primaryColor,
-                  activeColor: Styles.secondColor,
-                  inactiveColor: Styles.secondColor,
-                  onChanged: (double level) {
-                    context
-                        .read<ColorBrightnessLevelBloc>()
-                        .add(BrightnessLevelSetEvent(level: level));
-                  }),
-            ),
-          ),
-        )
+        const SliderBrightnessLevel()
       ],
+    );
+  }
+}
+
+class SliderBrightnessLevel extends StatefulWidget {
+  const SliderBrightnessLevel({Key? key}) : super(key: key);
+
+  @override
+  State<SliderBrightnessLevel> createState() => _SliderBrightnessLevelState();
+}
+
+class _SliderBrightnessLevelState extends State<SliderBrightnessLevel> {
+  @override
+  Widget build(BuildContext context) {
+    return SliderTheme(
+      data: SliderThemeData(overlayShape: SliderComponentShape.noOverlay),
+      child: BlocProvider(
+        create: (context) => ColorBrightnessLevelBloc(),
+        child: BlocBuilder<ColorBrightnessLevelBloc, double>(
+          builder: (context, currentLevel) => Slider.adaptive(
+              value: currentLevel,
+              min: BrightnessLevel.minLevel,
+              max: BrightnessLevel.maxLevel,
+              thumbColor: Styles.primaryColor,
+              activeColor: Styles.secondColor,
+              inactiveColor: Styles.secondColor,
+              onChanged: (double level) {
+                context
+                    .read<ColorBrightnessLevelBloc>()
+                    .add(BrightnessLevelSetEvent(level: level));
+              }),
+        ),
+      ),
     );
   }
 }
